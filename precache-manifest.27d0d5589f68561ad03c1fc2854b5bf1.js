874a8d03981152dba641ac15b2d9af77cf35328a
Mateusz Michalak <michalakweb@gmail.com>
2019-04-05 12:01:52 +0200

    Updates

---
+++ precache-manifest.27d0d5589f68561ad03c1fc2854b5bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d6a1a78945675b864a",
+    "url": "/firstpwa/static/css/main.f74a07ee.chunk.css"
+  },
+  {
+    "revision": "c6d6a1a78945675b864a",
+    "url": "/firstpwa/static/js/main.3fceb3cd.chunk.js"
+  },
+  {
+    "revision": "56a98c85bda5afe8c7d4",
+    "url": "/firstpwa/static/js/runtime~main.b6e0e14e.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/firstpwa/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstpwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cfaf8964c4505a8c683eead126a2050b",
+    "url": "/firstpwa/index.html"
+  }
+];
